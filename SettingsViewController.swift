30e22973aae4326f686e52e68fcc99c7b5d6e65f
Ryan Newton <rnewton@hearsaycorp.com>
2015-01-23 13:32:53 -0800

    Initial commit, working tip calculator with settings & some UI

---
+++ SettingsViewController.swift
@@ -0,0 +1,84 @@
+//
+//  SettingsViewController.swift
+//  tipmunk
+//
+//  Created by Ryan Newton on 1/21/15.
+//  Copyright (c) 2015 ___rvkn___. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBOutlet weak var tipMinField: UITextField!
+    @IBOutlet weak var tipMaxField: UITextField!
+    @IBOutlet weak var tipDefaultField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var defaults = NSUserDefaults.standardUserDefaults()
+
+        let maxTip = Float(defaults.integerForKey("maxTip"))
+        let minTip = Float(defaults.integerForKey("minTip"))
+        let defaultTip = Float(defaults.integerForKey("defaultTip"))
+        
+        tipMinField.text = String(defaults.integerForKey("minTip"))
+        tipMaxField.text = String(defaults.integerForKey("maxTip"))
+        tipDefaultField.text = String(defaults.integerForKey("defaultTip"))
+    }
+    
+    func saveNewSettings() {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        
+        // In case the user leaves the field blank
+        if let newMinValue = tipMinField.text?.toInt() {
+            defaults.setInteger(newMinValue, forKey: "minTip")
+        } else {
+            defaults.setInteger(15, forKey: "minTip")
+        }
+        
+        if let newMaxValue = tipMaxField.text?.toInt() {
+            defaults.setInteger(newMaxValue, forKey: "maxTip")
+        } else {
+            defaults.setInteger(25, forKey: "maxTip")
+        }
+        
+        if let newDefaultValue = tipDefaultField.text?.toInt() {
+            defaults.setInteger(newDefaultValue, forKey: "defaultTip")
+        } else {
+            defaults.setInteger(18, forKey: "defaultTip")
+        }
+        
+        defaults.synchronize()
+    }
+
+    @IBAction func saveSettingsClicked(sender: AnyObject) {
+        saveNewSettings()
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func cancelClicked(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  tipmunk
+//
+//  Created by Ryan Newton on 1/19/15.
+//  Copyright (c) 2015 ___rvkn___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipValueSlider: UISlider!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        setSliderOptionsFromUserDefaults()
+        updateDisplayedValues()
+    }
+    
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    
+    func setSliderOptionsFromUserDefaults() {
+        // sets the min/max/default of tip slider based on the user's settings
+        
+        var defaults = NSUserDefaults.standardUserDefaults()
+        
+        var minTip = Float(defaults.integerForKey("minTip"))
+        var maxTip = Float(defaults.integerForKey("maxTip"))
+        var defaultTip = Float(defaults.integerForKey("defaultTip"))
+
+        tipValueSlider.minimumValue = minTip
+        tipValueSlider.maximumValue = maxTip
+        tipValueSlider.value = defaultTip
+    }
+    
+    func updateDisplayedValues() {
+        var intTipValue = Int(tipValueSlider.value)
+        let tipAmount = Double(tipValueSlider.value/100)
+
+        var billAmount = (billField.text as NSString).doubleValue
+        var tip = billAmount * tipAmount
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        tipPercentageLabel.text = "\(intTipValue)%"
+
+        self.totalLabel.alpha = 0
+        UIView.animateWithDuration(0.5, animations: {
+            self.totalLabel.alpha = 1
+        })
+    }
+    
+    @IBAction func billAmountFieldEntered(sender: AnyObject) {
+        // clear the bill amount when the user enteres the field
+        billField.text = ""
+    }
+    
+    
+    @IBAction func onAmountFieldChanged(sender: AnyObject) {
+        updateDisplayedValues()
+    }
+    
+    @IBAction func tipSliderChanged(sender: UISlider) {
+        updateDisplayedValues()
+    }
+
+    
+    @IBAction func onTap(sender: AnyObject) {
+        // Allows the user to tap out of the keyboard
+        view.endEditing(true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
